474cd903f0441c52e3f31e9909fefeeb89ddf122
sanjaysengar25 <sanjaysengar15@gmail.com>
2021-10-07 16:40:54 +0530

    initial commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index($id)
+    {
+        try {
+         $post = Post::findOrFail($id);
+         if ($post) {
+            return  $post = Post::findOrFail($id);
+        }
+    } catch (\Exception $e) {
+        return response()->json(['status' => 'error', 'message' => $e->getMessage()]);
+    }
+
+    }
+
+    public function show()
+    {
+       
+       return Post::all();
+    }
+    
+    
+    public function store(Request $request)
+    {
+        try {
+            $post = new Post();
+            $post->title = $request->title;
+            $post->body = $request->body;
+
+            if ($post->save()) {
+                return response()->json(['status' => 'success', 'message' => 'Post created successfully']);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['status' => 'error', 'message' => $e->getMessage()]);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            $post = Post::findOrFail($id);
+            $post->title = $request->title;
+            $post->body = $request->body;
+
+            if ($post->save()) {
+                return response()->json(['status' => 'success', 'message' => 'Post updated successfully']);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['status' => 'error', 'message' => $e->getMessage()]);
+        }
+    }
+
+    public function destroy($id)
+    {
+        try {
+            $post = Post::findOrFail($id);
+
+            if ($post->delete()) {
+                return response()->json(['status' => 'success', 'message' => 'Post deleted successfully']);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['status' => 'error', 'message' => $e->getMessage()]);
+        }
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->post('/register','AuthController@register');
+    $router->post('/login','AuthController@login');
+
+    $router->group(['middleware' => 'auth'], function () use ($router) {
+        $router->post('/logout','AuthController@logout');
+        $router->get('/posts/{id}','PostController@index');
+        $router->get('/posts','PostController@show');
+        $router->post('/posts','PostController@store');
+        $router->post('/posts-edit/{id}','PostController@update');
+        $router->post('/posts/{id}','PostController@destroy');
+    });
+});
