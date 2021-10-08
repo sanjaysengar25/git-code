469d57fcfe89e453034b3096868ac32868736b9c
sanjaysengar25 <92076126+sanjaysengar25@users.noreply.github.com>
2021-10-08 12:15:08 +0530

    Add files via upload

---
+++ ExampleController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ExampleController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+      return "dfvdvdv df f f gbf f f";
+    }
+
+
+}
